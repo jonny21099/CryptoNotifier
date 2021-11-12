25ac37c36f1c1660f831a6afda834fe8ed4477dd
jonny21099 <43177180+jonny21099@users.noreply.github.com>
2021-11-11 21:53:59 -0700

    Refined tech debt (#23)
    * Refined tech debt
    
    * Update all log messages to use fstrings

--- src/coinmarketcap/crypto.py
+++ src/coinmarketcap/crypto.py
@@ -4,32 +4,32 @@ import requests
 
 class Crypto:
     def __init__(self, environment):
-        self.api_key = environment["api_key"]
-        self.currency = environment["currency"]
-        self.cryptos = environment["cryptos"]
+        self.__api_key = environment["api_key"]
+        self.__currency = environment["currency"]
+        self.__cryptos = environment["cryptos"]
 
     def get_crypto_quote(self):
         current_price_list = []
 
         URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-        PARAMS = {"CMC_PRO_API_KEY": self.api_key, "symbol": ",".join(self.cryptos), "convert": self.currency}
+        PARAMS = {"CMC_PRO_API_KEY": self.__api_key, "symbol": ",".join(self.__cryptos), "convert": self.__currency}
 
         print("Fetching prices...")
         r = requests.get(URL, PARAMS)
         data = r.json()
-        print("Retrieval time: {}\n".format(datetime.now()))
+        print(f"Retrieval time: {datetime.now()}\n")
 
-        for crypto in self.cryptos:
-            price = data['data'][crypto]["quote"][self.currency]["price"]
+        for crypto in self.__cryptos:
+            price = data['data'][crypto]["quote"][self.__currency]["price"]
             current_price_list.append(price)
             print(self.format_log_message(crypto, price))
 
         return current_price_list
 
     def format_log_message(self, crypto, price):
-        message = "Asset: {}\n".format(crypto) \
-                  + "Currency: {}\n".format(self.currency) \
-                  + "Price: ${}\n".format(price)
+        message = f"Asset: {crypto}\n" \
+                  + f"Currency: {self.__currency}\n" \
+                  + f"Price: ${price}\n"
         return message
 
 
